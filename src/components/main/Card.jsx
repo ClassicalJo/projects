07fb6099b136fd67e545da4772adfa388ee23001
ClassicalJo <jb.barrientos@yahoo.com>
2022-01-27 04:48:13 -0300

    full functionality

--- src/components/main/Card.jsx
+++ src/components/main/Card.jsx
@@ -5,18 +5,25 @@ import useExpand from "./useExpand"
 export default function Card({ time, content }) {
     let [appear, setAppear] = useState('')
     let { expand, reduce, toggle } = useExpand()
-    let wrapper = 'project-card-wrapper ' + appear + (expand ? ' expanded' : '')
-    let card = 'project-card' + (expand ? ' expanded-height' : '') + (reduce ? " reduce" : '')
-    let { title } = content
     useEffect(() => {
         let timeout = setTimeout(() => setAppear('appear'), time)
         return () => clearTimeout(timeout)
     }, [])
+    
+    let className = () => {
+        let str = 'project-card-wrapper '
+        if (reduce) return str + 'reduced'
+        if (expand) return str + 'expanded'
+        else return str
+    }
+
+    let { title } = content
+    
     return (
         <div
             onClick={toggle}
-            className={wrapper}  >
-            <div className={card} >
+            className={className()}  >
+            <div className={'project-card ' + appear} >
                 <div className="image-wrapper">
                     <img src="https://via.placeholder.com/350"></img>
                 </div>
---
+++ src/components/main/Container.jsx
@@ -0,0 +1,9 @@
+import Grid from "./Grid";
+
+export default function Container() {
+    return (
+        <div className="container">
+            <Grid />
+        </div>
+    )
+}
