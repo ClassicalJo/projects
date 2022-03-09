cb9e6bde565c52f6f8c6437cad51835b112351aa
ClassicalJo <jb.barrientos@yahoo.com>
2022-03-09 15:58:03 -0300

    Removed unnecessary vars due to not preloading videos in component

--- src/components/common/Loader.jsx
+++ src/components/common/Loader.jsx
@@ -3,12 +3,9 @@ import Loading from "./Loading";
 import WebFont from "webfontloader";
 import Main from '../main/Main'
 
-let importAll = r => r.keys().map(r)
-const webms = importAll(require.context('../../assets/projects/webms'))
-
 export default function Loader() {
     let [ready, setReady] = useState(false)
-    let [done, setDone] = useState(false)
+    
     useEffect(() => {
         WebFont.load({
             google: {
